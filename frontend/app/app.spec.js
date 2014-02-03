c8a603efcf1576cad9628c73097adc59d6e23d73
Martin Gontovnikas <martin@gon.to>
2014-02-03 00:59:01 -0300

    Fixing tests

--- frontend/app/app.spec.js
+++ frontend/app/app.spec.js
@@ -2,7 +2,7 @@ describe( 'AppCtrl', function() {
   describe( 'isCurrentUrl', function() {
     var AppCtrl, $location, $scope;
 
-    beforeEach( module( 'ngBoilerplate' ) );
+    beforeEach( module( 'meaner' ) );
 
     beforeEach( inject( function( $controller, _$location_, $rootScope ) {
       $location = _$location_;
